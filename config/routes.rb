e395b14134699c2b62d981a2ea52cf6c550d0fcc
andresromero80 <andresromero80@hotmail.com>
2018-12-02 23:46:49 +0100

    Added conversations and messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  # get 'messages/index'
+  # get 'conversations/index'
+
+   resources :conversations do
+    resources :messages
+   end
+
   resources :trades
   devise_for :users
   # get 'static_pages/home'
   # get 'static_pages/index'
 	root to: "static_pages#home"
 end
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_210501) do
+ActiveRecord::Schema.define(version: 2018_12_02_220909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,23 @@ ActiveRecord::Schema.define(version: 2018_12_02_210501) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "conversation_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "trades", force: :cascade do |t|
