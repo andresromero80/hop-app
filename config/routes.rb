713a23d3bcef90cb06003751d6569aab8e435353
andresromero80 <andresromero80@hotmail.com>
2018-12-02 22:22:58 +0100

    added static_pages (home, index); activestorage; trades

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-	root to: "application#hello"
+  resources :trades
+  devise_for :users
+  # get 'static_pages/home'
+  # get 'static_pages/index'
+	root to: "static_pages#home"
 end
---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get static_pages_index_url
+    assert_response :success
+  end
+
+end
