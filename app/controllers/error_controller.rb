c6680872725f0f09969647610805b29dc96d6f9e
malo2k <malo2k@laposte.net>
2018-12-05 14:21:25 +0100

    add profile pages

---
+++ app/controllers/error_controller.rb
@@ -0,0 +1,4 @@
+class ErrorController < ApplicationController
+	def error_404
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,11 @@ Rails.application.routes.draw do
 
   resources :trades, :products, :categories
   devise_for :users
-	root to: "static_pages#index"
-  get 'customer/signup'
-  get 'customer/login'
+  root to: "static_pages#index"
+  get '/customer/signup'
+  get '/customer/login'
+  get '/profile/account', to: 'profile#account'
+  get 'messages/index', to: 'messages#index'
 end
 
 
