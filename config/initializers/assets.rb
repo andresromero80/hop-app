00648fba300e6aa51b817943ce0416152d186329
andresromero80 <andresromero80@hotmail.com>
2018-12-10 11:06:08 +0100

    modif application.scss, added images lines to main.scss.erb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,6 +8,8 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join("vendor")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "extras")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "img", "bg")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
