b7bc0370dbdd66b23d45f7f5f7dcdb4c526f7a35
alexandrePeccard <alexandre.peccard@gmail.com>
2018-12-11 22:22:25 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,10 @@
 require 'faker'
 
 
+
 #Create address
 3.times do |index|
-	address = Address.create!(street_number: "13", street: 'rue des hortensias', city: 'ploufragan', postal_code: '22440', country: "France")
+	address = Address.create!(street_number: "123", street: Faker::StarWars.planet, city: Faker::StarWars.planet, postal_code: Faker::Address.building_number, country: "France")
 		
 end 
 
