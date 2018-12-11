45d46a4e03b75ed92b060b25fe1e443e71582c6c
andresromero80 <andresromero80@hotmail.com>
2018-12-11 10:41:04 +0100

    added route vend/assets/img/featured

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.paths << Rails.root.join("vendor")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "extras")
 Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "img", "bg")
+Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "img", "featured")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
