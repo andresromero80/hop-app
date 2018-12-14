042e7bc7c594247b060b40fb668bd1179de27b53
andresromero80 <andresromero80@hotmail.com>
2018-12-14 23:11:49 +0100

    production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,7 +104,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   # config.action_mailer.default_url_options = { host: 'hop-app-thp.herokuapp.com', port: 587 }
-  config.action_mailer.default_url_options = { :host => 'hop-app-thp.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'hoproduce.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
