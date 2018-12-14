8c8013b449ce6b7bea47b0c199565ff7fa2d2727
andresromero80 <andresromero80@hotmail.com>
2018-12-14 09:10:57 +0100

    modif url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,7 +103,8 @@ Rails.application.configure do
   # }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'hop-app-thp.herokuapp.com' }
+  # config.action_mailer.default_url_options = { host: 'hop-app-thp.herokuapp.com', port: 587 }
+  config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
 
   config.action_mailer.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
@@ -114,4 +115,10 @@ Rails.application.configure do
       :password       => ENV['SENDGRID_PASSWORD'],
 
     }
+
+     
+    #To enable sending emails in dev
+  config.action_mailer.perform_deliveries = true
+
+
 end
